fb65b15877706e3c4c77a6c37f041a4ab65edca9
Barbara Martin Brown <bmartin-@c2r5s3.42malaga.com>
2025-05-21 19:50:21 +0200

    add Makefile printf.h

---
+++ ft_printf_p/printf.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   printf.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bmartin- <bmartin-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/22 16:25:51 by bmartin-          #+#    #+#             */
+/*   Updated: 2025/05/21 19:41:51 by bmartin-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PRINTF_H
+# define PRINTF_H
+# include <unistd.h>
+# include <stdlib.h>
+# include <limits.h>
+# include <stdio.h>
+# include <stdarg.h>
+
+char	*ft_converse_base(unsigned long n, char *base_str);
+int		ft_count_caracter(char *str);
+int		ft_count_digit(unsigned long n, int base);
+char	*ft_format(char specifier, va_list args);
+int		ft_format_int(const char *format, int *i, va_list args);
+char	*ft_itoa(int n);
+int		ft_putchar(char c);
+int		ft_putstr(char *s);
+int		ft_printf(const char *format, ...);
+
+#endif
