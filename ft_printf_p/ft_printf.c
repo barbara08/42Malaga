074ef47ea81a7b381a5d42df1de1978d00242da6
Barbara Martin Brown <bmartin-@c1r3s5.42malaga.com>
2025-05-29 19:53:55 +0200

    more modified to try

--- ft_printf_p/ft_printf.c
+++ ft_printf_p/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: bmartin- <bmartin-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/19 15:13:27 by bmartin-          #+#    #+#             */
-/*   Updated: 2025/05/29 19:46:39 by bmartin-         ###   ########.fr       */
+/*   Updated: 2025/05/29 19:51:57 by bmartin-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,6 +45,7 @@ int main(void)
 	ft_printf("All the format to try:\n");
 	ft_printf("Im %s, my age is %d and my pointer is %p.\n", name, age, ptr);
 	ft_printf("%%u => %u, %%x => %x, %%X => %X \n", 4294967295, 4125, 4125);
+	ft_printf("%%u => %u, %%x => %x, %%X => %X \n", 48754, 824, 824);
 	ft_printf("Este es: %% \n");
 
 	return (0);
