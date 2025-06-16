42f2475909ecf7f8cf395070292722353e734a02
Bárbara <barbara.martin.brown@gmail.com>
2025-06-16 19:55:20 +0200

    more functions

--- exam_p/rev_print.c
+++ exam_p/rev_print.c
@@ -14,3 +14,26 @@ LIOP a 0bud$
 $> ./rev_print | cat -e
 $
 */
+
+void rev_print(char *str)
+{
+    int i = 0;
+    while (str[i])
+        i++;
+    i--;
+    while (i >=0)
+    {
+        write(1, &str[i], 1);
+        i--;
+    }
+}
+
+int main(int argc, char **argv)
+{
+    if (argc == 2)
+        rev_print(argv[1]);
+    else
+        printf("Write a phrase");
+    write(1, "\n", 1);
+    return(0);
+}
