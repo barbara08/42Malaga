de86dac2a10f5a602b9bceda062f2dfd241fa25d
Barbara Martin Brown <bmartin-@c2r2s4.42malaga.com>
2025-07-02 13:22:47 +0200

    add new functions

---
+++ exam_p/level_2/alpha_mirror.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/*
+Write a program called alpha_mirror that takes a string and displays this string
+after replacing each alphabetical character by the opposite alphabetical
+character, followed by a newline.
+
+'a' becomes 'z', 'Z' becomes 'A'
+'d' becomes 'w', 'M' becomes 'N'
+
+and so on.
+
+Case is not changed.
+
+If the number of arguments is not 1, display only a newline.
+
+Examples:
+
+$>./alpha_mirror "abc"
+zyx
+$>./alpha_mirror "My horse is Amazing." | cat -e
+Nb slihv rh Znzarmt.$
+$>./alpha_mirror | cat -e
+$
+$>
+*/
+void ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+int main(int argc, char **argv)
+{
+	int i = 0;
+	if (argc == 2)
+	{
+		while(argv[1][i])
+		{
+			if(argv[1][i] >= 'A' && argv[1][i] <= 'Z')
+				argv[1][i] = 'Z' - argv[1][i] + 'A';
+			else if(argv[1][i] >= 'a' && argv[1][i] <= 'z')
+				argv[1][i] = 'z' - argv[1][i] + 'a';
+			//write(1, &argv[1][i], 1);
+			ft_putchar(argv[1][i]);
+			i++;
+		}
+	}
+	return(0);
+}
+
+
