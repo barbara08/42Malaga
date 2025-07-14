d580a5f163c4a513ed8bc82c8e0880b41ee63b2f
Barbara Martin Brown <bmartin-@c3r1s3.42malaga.com>
2025-07-14 13:07:14 +0200

    start push_swap

---
+++ push_swap/push_swap.h
@@ -0,0 +1,10 @@
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+# include <unistd.h>
+# include <stdlib.h>
+# include <limits.h>
+# include <stdio.h>
+# include <stdarg.h>
+
+
+#endif
---
+++ push_swap/rules.c
@@ -0,0 +1,11 @@
+#include "push_swap.h"
+
+
+
+
+/* 
+sa swap a: Intercambia los dos primeros elementos del stack a.
+No hace nada si hay uno o menos elementos.
+*/
+
+
